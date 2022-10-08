e7d7f8aeb7a51e1a57fda409d23c546ccc56a644
gus-codes <gusmnk.dev@gmail.com>
2022-10-08 03:01:59 -0300

    first

--- main.js
+++ main.js
@@ -1,54 +1,105 @@
-const timer ={
-    pomodoro: 25,
-    shortBreak:  5,
-    longBreak: 15,
-    longBreakInterval: 4
+const timer = {
+  pomodoro: 25,
+  shortBreak: 5,
+  longBreak: 15,
+  longBreakInterval: 4,
 };
 
 let interval;
 
 
+const mainButton = document.getElementById('js-btn');
+mainButton.addEventListener('click', () => {
+    const {action} = mainButton.dataset;
+    if(action === 'start'){
+        startTimer();
+    } else {
+        stopTimer()
+    }
+});
+
 const modeButtons = document.querySelector("#js-mode-buttons");
 
-modeButtons.addEventListener('click', handleMode);
+modeButtons.addEventListener("click", handleMode);
 
-function handleMode(event){
+function handleMode(event) {
+  const { mode } = event.target.dataset;
 
-    const {mode} = event.target.dataset;
+  if (!mode) return;
 
-    if(!mode) return;
+  switchMode(mode);
+  stopTimer();
+}
 
-    switchMode(mode)
-};
+function switchMode(mode) {
+  timer.mode = mode;
+  timer.remainingTime = {
+    total: timer[mode] * 60,
+    minutes: timer[mode],
+    seconds: 0,
+  };
 
-    function switchMode(mode){
-        timer.mode = mode;
-        timer.remainingTime = {
-            total: timer[mode] * 60,
-            minutes: timer[mode],
-            seconds:  0,
-        };
+  document
+    .querySelectorAll("button[data-mode]")
+    .forEach((e) => e.classList.remove("active"));
+  document.querySelector(`[data-mode="${mode}"]`).classList.add("active");
+  document.body.style.backgroundColor = `var(--${mode})`;
 
-            document
-                .querySelectorAll('button[data-mode]')
-                .forEach( e => e.classList.remove('active'));
-            document.querySelector(`[data-mode="${mode}"]`).classList.add('active');
-            document.body.style.backgroundColor = `var(--${mode})`;
+  updateClock();
+}
 
+function getRemainingTime(endTime) {
+  const currentTime = Date.parse(new Date());
+  const difference = endTime - currentTime;
 
-        updateClock();
+  const total = Number.parseInt(difference / 1000, 10);
+  const minutes = Number.parseInt((total / 60) % 60, 10);
+  const seconds = Number.parseInt(total % 60, 10);
 
-    };
+  return {
+    total,
+    minutes,
+    seconds,
+  };
+}
 
-    function updateClock(){
-        const {remainingTime} = timer;
-        const minutes = `${remainingTime.minutes}`.padStart(2, '0');
-        const seconds =  `${remainingTime.seconds}`.padStart(2, '0');
+function startTimer() {
+  let { total } = timer.remainingTime;
+  const endTime = Date.parse(new Date()) + total * 1000;
 
-        const min = document.getElementById('js-minutes');
+  mainButton.dataset.action = 'stop';
+  mainButton.textContent = 'stop';
+  mainButton.classList.add('active');
 
-        const sec = document.getElementById('js-seconds');
+  interval = setInterval(function () {
+    timer.remainingTime = getRemainingTime(endTime);
+    updateClock();
 
-        min.textContent = minutes;
-        sec.textContent = seconds;
+    total = timer.remainingTime.total;
+    if (total <= 0) {
+      clearInterval(interval);
     }
+  }, 1000);
+}
+
+
+function stopTimer(){
+    clearInterval(interval);
+
+    mainButton.dataset.action = 'start';
+    mainButton.textContent = 'start';
+    mainButton.classList.remove('active');
+}
+
+function updateClock() {
+  const { remainingTime } = timer;
+  const minutes = `${remainingTime.minutes}`.padStart(2, "0");
+  const seconds = `${remainingTime.seconds}`.padStart(2, "0");
+
+  const min = document.getElementById("js-minutes");
+
+  const sec = document.getElementById("js-seconds");
+
+  min.textContent = minutes;
+  sec.textContent = seconds;
+}
